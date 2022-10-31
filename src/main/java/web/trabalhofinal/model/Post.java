d673543997ccd1776ddca7e358f5c88632bc643c
Emanuelle-Oliveira <manutrindadeoliver@gmail.com>
2022-10-31 18:55:40 -0300

    classe post criada

--- src/main/java/web/trabalhofinal/model/Post.java
+++ src/main/java/web/trabalhofinal/model/Post.java
@@ -1,5 +1,76 @@
 package web.trabalhofinal.model;
 
-public class Post {
+import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "post")
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String descricao;
+	@Enumerated(EnumType.STRING)
+	private Categoria categoria;
+	private LocalDate data;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_cliente")
+	private Cliente cliente;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public LocalDate getData() {
+		return data;
+	}
+	
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", descricao=" + descricao + ", categoria=" + categoria + ", data=" + data
+				+ ", cliente=" + cliente + "]";
+	}
 }
--- src/main/java/web/trabalhofinal/repository/UsuarioRepository.java
+++ src/main/java/web/trabalhofinal/repository/UsuarioRepository.java
@@ -1,5 +1,9 @@
 package web.trabalhofinal.repository;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import web.trabalhofinal.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository <Usuario, Long>{
 
 }
